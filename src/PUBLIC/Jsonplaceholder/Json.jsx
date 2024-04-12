13ea447defef180f9c1d5a554aa23b5ffeb44743
khushigoyal387 <khushigoyal387@gmail.com>
2024-04-12 16:30:35 +0530

    first

--- src/PUBLIC/Jsonplaceholder/Json.jsx
+++ src/PUBLIC/Jsonplaceholder/Json.jsx
@@ -6,12 +6,15 @@ function Posts() {
   const totalPosts = 100;
   const postsPerPage = 10;
   const [currentPage, setCurrentPage] = useState(1);
+  const [previousButton, setPreviousButton] = useState();
 
   useEffect(() => {
     axios.get(`https://jsonplaceholder.typicode.com/posts?_page=${currentPage}`)
       .then(response => {
-        setPosts(prevPosts => [...prevPosts, ...response.data]);
+        setPosts(response.data);
+        setPreviousButton (currentPage>1);
       })
+
       .catch(error => {
         console.error('Error fetching posts:', error);
       });
@@ -23,18 +26,34 @@ function Posts() {
     }
   };
 
+  const previousPosts = () => {
+    if (currentPage > 1) {
+      setCurrentPage(prevPage => prevPage - 1);
+    }
+  };
+
   return (
+
     <div>
+
       <h1>Posts</h1>
+
       <ul>
         {posts.map(post => (
           <li key={post.id}>{post.title}</li>
         ))}
       </ul>
+
       {posts.length < totalPosts && (
         <button onClick={loadMorePosts}>Load More</button>
       )}
+
+       {setPreviousButton && (
+        <button onClick={previousPosts}>Previous</button>
+      )}
+
     </div>
+
   );
 }
 
